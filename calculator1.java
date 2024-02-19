03927758bb6c56d60fcbb70b46579805cc332357
aditya <01fe22bca055@kletech.ac.in>
2024-02-19 16:39:43 +0530

    updated  the extended code

--- calculator1.java
+++ calculator1.java
@@ -1,3 +1,5 @@
+package calculator1;
+
 import java.util.Scanner;
 
 public class calculator1 {
---
+++ ext_cal.java
@@ -0,0 +1,18 @@
+package calculator1;
+
+
+
+class ext_cal{
+    float square(float num1)
+      {
+     return(num1*num1);
+      }
+ 
+   float cube(float num1)
+       {
+     return(num1*num1*num1);
+       }
+}
+ 
+    
+
